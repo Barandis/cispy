f1b3e966f0d8d2ff8e262976ea9d0ffc90579073
Barandis <barandis1729@gmail.com>
2017-08-17 10:49:19 -0400

    Added JSDoc for operations, protocol

--- src/core/operations.js
+++ src/core/operations.js
@@ -19,57 +19,101 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// operations.js
-// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * Provides basic channel operations for puts, takes, and alts.
+ *
+ * @module cispy/core/operations
+ * @private
+ */
 
 const { box, isBox, DEFAULT } = require('./channel');
 
-// These two handlers are used by channels to track execution of instructions (put, take, and alts). They provide two
-// pieces of information: the function to call when a put or take unblocks (because a value sent to put has been taken,
-// or a take has accepted a value that has been put) and whether or not the handler is still active.
-//
-// The function is a callback that actually defines the difference between put/take and putAsync/takeAsync:
-// while the unblocked calls use the callback passed to the function, put and take simply continue the process where it
-// left off. (This is why put and take only work inside go functions, because otherwise there's no process to continue.)
-// The alts instruction always continues the process upon completion; there is no unblocked version of alts.
-//
-// This function is provided as the return value of the commit method. Calling commit has no extra effect with put and
-// take instructions, but for alts, it also marks the handler as no longer being active. This means that only one of
-// the operations passed to alts can be completed, because after the first one, the handler is no longer active and
-// will not be allowed to process a second operation.
-//
-// If a put or take (or equivalent alts operation) cannot be immediately completed because there isn't a corresponding
-// pending take or put, the handler is queued to be run when a new take or put becomes available.
+/**
+ * These two handlers are used by channels to track execution of instructions (put, take, and alts). They provide two
+ * pieces of information: the function to call when a put or take unblocks (because a value sent to put has been taken,
+ * or a take has accepted a value that has been put) and whether or not the handler is still active.
+ *
+ * The function is a callback that actually defines the difference between put/take and putAsync/takeAsync:
+ * while the unblocked calls use the callback passed to the function, put and take simply continue the process where it
+ * left off. (This is why put and take only work inside go functions, because otherwise there's no process to continue.)
+ * The alts instruction always continues the process upon completion; there is no unblocked version of alts.
+ *
+ * This function is provided as the return value of the commit method. Calling commit has no extra effect with put and
+ * take instructions, but for alts, it also marks the handler as no longer being active. This means that only one of
+ * the operations passed to alts can be completed, because after the first one, the handler is no longer active and
+ * will not be allowed to process a second operation.
+ *
+ * If a put or take (or equivalent alts operation) cannot be immediately completed because there isn't a corresponding
+ * pending take or put, the handler is queued to be run when a new take or put becomes available.
+ *
+ * @typedef Handler
+ * @property {boolean} active Whether or not the operation is still active. An inactive operation is not serviced and
+ *     will be cleared from the queue on the next dirty operation purge.
+ * @property {function} commit Marks the handler as inactive (so it doesn't run twice) and returns the callback to be
+ *     run when the operation completes.
+ * @private
+ */
+
+/**
+ * A callback function run when a put or take operation completes.
+ *
+ * @callback HandlerCallback
+ * @param {*} value The value provided by the channel. In a take, this is the value taken from the channel. In a put,
+ *     this is `true` for a successful put and `false` if the channel is closed before the put can complete.
+ * @private
+ */
+
+/**
+ * Creates a new handler used for put and take operations. This handler is always active, as there will never be a time
+ * when it's in the queue but already handled.
+ *
+ * @param {module:cispy/core/operations~HandlerCallback} fn The callback to be run when the put or take operation
+ *     completes.
+ * @return {module:cispy/core/operations~Handler} The new handler.
+ */
 function opHandler(fn) {
   return {
-    fn,
-    active: true,
-
-    commit() {
-      return this.fn;
-    }
-  };
-}
-
-function altsHandler(active, fn) {
-  return {
-    a: active,
-    fn,
-
     get active() {
-      return this.a.value;
+      return true;
     },
 
     commit() {
-      this.a.value = false;
-      return this.fn;
+      return fn;
     }
   };
 }
 
-// Creates an array of values from 0 to n - 1, shuffled randomly. Used to randomly determine the priority of operations
-// in an alts call.
+/**
+ * Creates a new handler used for alts operations.
+ *
+ * @param {module:cispy/core/channel~Box} active A boxed value indicating whether the handler is valid. This is a boxed
+ *     value because the alts-handling code needs to manipulate it directly; this could probably be improved.
+ * @param {module:cispy/core/operations~HandlerCallback} fn The callback to be run when (and if) the operation
+ *     completes.
+ * @return {module:cispy/core/operations~Handler} The new handler.
+ */
+function altsHandler(active, fn) {
+  return {
+    get active() {
+      return active.value;
+    },
+
+    commit() {
+      active.value = false;
+      return fn;
+    }
+  };
+}
+
+/**
+ * Creates an array of values from 0 to n - 1, shuffled randomly. Used to randomly determine the priority of operations
+ * in an alts operation.
+ *
+ * @param  {number} n The upper (exclusive) bound for the random numbers. This ends up also being the length of the
+ *     resulting array.
+ * @return {number[]} An array of numbers from 0 to n - 1, arranged randomly.
+ * @private
+ */
 function randomArray(n) {
   const a = [];
   for (let k = 0; k < n; ++k) {
@@ -84,15 +128,40 @@ function randomArray(n) {
   return a;
 }
 
-// Processes the operations in an alts function call. This works in the same way as `takeAsync` and `putAsync`
-// except that each operation (each of which can be either a put or a take on any channel) is queued in a random order
-// onto its channel and only the first to complete returns a value (the other ones become invalidated then and are
-// discarded).
-//
-// The callback receives an object instead of a value. This object has two properties: `value` is the value that was
-// returned from the channel, and `channel` is the channel onto which the successful operation was queued.
-//
-// The `options` parameter is the same as the options parameter in `alts`.
+/**
+ * A callback invoked by {@link module:cispy/core/operastions~altsAsync} when an operation completes.
+ *
+ * @callback altsCallback
+ * @param {*} obj.value The value returned from the channel. For a take, this is the value taken from the channel. For a
+ *     put, it's `true` if the put was successful or `false` if the channel was closed before the put completed.
+ * @param {module:cispy/core/channel~Channel} obj.channel The channel upon which the completed operation acted.
+ * @private
+ */
+
+/**
+ * Processes the operations in an alts function call. This works in the same way as `takeAsync` and `putAsync`
+ * except that each operation (each of which can be either a put or a take on any channel) is queued in a random order
+ * onto its channel and only the first to complete returns a value (the other ones become invalidated then and are
+ * discarded).
+ *
+ * The callback receives an object instead of a value. This object has two properties: `value` is the value that was
+ * returned from the channel, and `channel` is the channel onto which the successful operation was queued.
+ *
+ * The `options` parameter is the same as the options parameter in `alts`.
+ *
+ * @param {(module:cispy/core/channel~Channel|[])[]} ops The operations governed by the alts operation. The first of
+ *     these to complete is successful; the rest are discarded. Each element of the array represents an operation. If
+ *     the element is a channel, then it's a take operation on that channel. If the element is a two-element array of
+ *     channel and value, then it's a put operation putting that value on that channel.
+ * @param {module:cispy/core/operations~altsCallback} callback A function called when one of the operations completes.
+ * @param {boolean} [options.priority=false] Indicates the priority given to listed operations. If this is `true`, then
+ *     operations listed first in the array have priority over those listed later, if multiple operations will complete
+ *     immediately without blocking. If this is `false`, then the priority is random.
+ * @param {*} [options.default] The value to return from this call if all operations block. This behavior *only* happens
+ *     if this option is present; otherwise, the call blocks until an operation completes. If this *is* present, the
+ *     channel provided to the callback is {@link module:cispy~DEFAULT|DEFAULT}.
+ * @private
+ */
 function altsAsync(ops, callback, options) {
   const count = ops.length;
   if (count === 0) {
@@ -151,8 +220,24 @@ function altsAsync(ops, callback, options) {
   }
 }
 
-// Puts a value onto a channel. When the value is successfully taken off the channel by another process or when
-// the channel closes, the callback fires if it exists.
+/**
+ * A function called when a put or a take completes.
+ *
+ * @callback opCallback
+ * @param {*} value The value that comes from the operation call. If it's a take, this is the value taken from the
+ *     channel. If it's a put, this is `true` if the put succeeded or `false` if the channel closed.
+ * @private
+ */
+
+/**
+ * Puts a value onto a channel. When the value is successfully taken off the channel by another process or when
+ * the channel closes, the callback fires if it exists.
+ *
+ * @param {module:cispy/core/channel~Channel} channel The channel onto which a value is being put.
+ * @param {*} value The value being put onto the channel.
+ * @param {module:cispy/core/operations~opCallback} [callback] The function called when the put completes.
+ * @private
+ */
 function putAsync(channel, value, callback) {
   const result = channel.put(value, opHandler(callback));
   if (result && callback) {
@@ -160,7 +245,12 @@ function putAsync(channel, value, callback) {
   }
 }
 
-// Takes a value off a channel. When the value becomes available, it is passed to the callback.
+/**
+ * Takes a value off a channel. When the value becomes available, it is passed to the callback.
+ *
+ * @param  {module:cispy/core/channel~Channel} channel The channel from which a value is being taken.
+ * @param  {module:cispy/core/operations~opCallback} [callback] The function called when the take completes.
+ */
 function takeAsync(channel, callback) {
   const result = channel.take(opHandler(callback));
   if (result && callback) {
--- src/core/protocol.js
+++ src/core/protocol.js
@@ -19,25 +19,62 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// protocol.js
-// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Protocols for iteration and reduction. The source for these protocols depends on which protocol it is.
-//
-// Iteration: a part of the ES6 standard.
-// Transduction: agreed to by several parties who maintain transducer libraries in the comment thread for an issue on
-//     one of them (https://github.com/cognitect-labs/transducers-js/issues/20).
+/**
+ * Protocols for iteration and reduction. The source for these protocols depends on which protocol it is.
+ *
+ * * **Iteration:** a part of the ES6 standard.
+ * * **Transduction:** agreed to by several parties who maintain transducer libraries in the
+ *   [comment thread](https://github.com/cognitect-labs/transducers-js/issues/20) for an issue on one of them.
+ *
+ * @mocule cispy/core/protocol
+ * @private
+ */
 
+/**
+ * Determines whether Symbols should be used as the names of properties related to the protocol, assuming Symbols are
+ * available in the environment.
+ *
+ * **This is FALSE temporarily.** My own transducer library is using non-Symbols. I intend to change that...we're into
+ * ES7 now, so it seems like using ES6 features should be reasonable. When it changes in that library, it'll also
+ * change here.
+ *
+ * @type {boolean}
+ * @private
+ */
 const USE_SYMBOLS = false;
+
+/**
+ * Whether or not to use Symbols. This is based on the value of
+ * {@link module:cispy/core/protocol~USE_SYMBOLS|USE_SYMBOLS} *and* on whether Symbols are available in the environment.
+ * If Symbols are unavailable, it doesn't matter what `USE_SYMBOLS` is set to, Symbols will not be used.
+ *
+ * @type {boolean}
+ * @private
+ */
 const symbol = typeof Symbol !== 'undefined';
 
-// Generation of the key used on an object to store a protocol function. This is a symbol if symbols are available and
-// USE_SYMBOLS (above) is set to true; if not, it's a regular string. If a symbol of the supplied name already exists,
-// it'll be used instead of having a new one generated.
+/**
+ * Generation of the key used on an object to store a protocol function. This is a Symbol if Symbols are available and
+ * {@link module:cispy/core/protocol~USE_SYMBOLS|USE_SYMBOLS} is set to true; if not, it's a regular string. If a Symbol
+ * of the supplied name already exists, it'll be used instead of having a new one generated.
+ *
+ * @param {string} name The name of the protocol function to generate a key for.
+ * @return {(string|Symbol)} The key to be used to store protocol values on objects. This is a Symbol if Symbols are
+ *     specified and exist in the environment; otherwise it is a string.
+ * @private
+ */
 function generateKey(name) {
   return USE_SYMBOLS && symbol ? Symbol.for(name) : `@@${name}`;
 }
 
+/**
+ * A mapping of easy-to-use names for protocol properties and the actual name of the property as it's stored on objects.
+ * This is merely for convenience, particularly since it's possible to use this library with Symbols or without,
+ * depending on environment.
+ *
+ * @type {Object}
+ * @private
+ */
 const protocols = {
   // Since this one is built in, it already has a custom Symbol property, so we don't need to generate a symbol for a
   // key when symbols are supported.
